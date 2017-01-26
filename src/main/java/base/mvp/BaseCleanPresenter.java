4374776487907d0472c73e434ca4ef31922f547b
chiara <chiaradia.juan@gmail.com>
2017-01-26 09:32:54 +0100

    BasePresenter: no need of a view reference on the presenter.

--- src/main/java/base/mvp/BaseCleanPresenter.java
+++ src/main/java/base/mvp/BaseCleanPresenter.java
@@ -4,16 +4,15 @@ import android.support.annotation.NonNull;
 
 import base.domain.UseCaseExecutor;
 
-public abstract class BaseCleanPresenter<V extends BaseContract.View, VM extends BaseContract.ViewModel> extends BasePresenter<V, VM> {
+public abstract class BaseCleanPresenter<VM extends BaseContract.ViewModel> extends BasePresenter<VM> {
 
     @NonNull
     protected final UseCaseExecutor mUseCaseExecutor;
 
     protected BaseCleanPresenter(
-            @NonNull V view,
             @NonNull VM viewModel,
             @NonNull UseCaseExecutor useCaseExecutor) {
-        super(view, viewModel);
+        super(viewModel);
         mUseCaseExecutor = useCaseExecutor;
     }
 }
